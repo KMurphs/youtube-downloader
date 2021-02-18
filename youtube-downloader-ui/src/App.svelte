05ec7dabb2cee54d067018912718c9ee7060c5a7
kmurphs <kibongesp@gmail.com>
2021-02-18 19:23:53 +0200

    Added frame for app ui

--- youtube-downloader-ui/src/App.svelte
+++ youtube-downloader-ui/src/App.svelte
@@ -2,29 +2,34 @@
 	export let name: string;
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<div class="app-container">
+	<header>My header</header>
+	<main>My Main Section</main>
+	<nav>My nav</nav>
+</div>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+.app-container{
+	width: 100%;
+	height: 100%;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	background-color: #eee;
+	position: relative;
+	z-index: 10;
+	/* border-radius: 15px; */
+}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+@media screen and (min-width: 1441px){
+	.app-container {
+		/* top: 19px; */
+		width: 1396px;
+		height: calc(100% - 38px);
 	}
+}
+
+
 </style>
