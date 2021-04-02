8bacb9605f282ae6bf172e1aebae911d9c1e668e
kmurphs <kibongesp@gmail.com>
2021-04-02 16:34:11 +0200

    Updated scheduler and task scripts

--- youtube_downloader/api/.scripts/download-task.bat
+++ youtube_downloader/api/.scripts/download-task.bat
@@ -1,4 +1,6 @@
 @echo off
 @REM cd ..
+CD %~dp0
+CD..
 venv\scripts\python app\downloader.py
 explorer videos
--- youtube_downloader/api/.scripts/setup-scheduler.bat
+++ youtube_downloader/api/.scripts/setup-scheduler.bat
@@ -1,6 +1,14 @@
+@ECHO OFF
 @REM Create Task
+
 @REM Command must be run with administrator priviledges
-SCHTASKS /CREATE /SC DAILY /TN "MyTasks\Youtube Downloader Task" /TR "C:\PersonalProjects\misc\YoutubeDownloader\app\scheduler\task.bat" /ST 01:00
+SCHTASKS /CREATE /SC DAILY /TN "MyTasks\Youtube Downloader Task" /TR "%~dp0download-task.bat" /ST 01:00
+
+@REM Setup venv
+CD %~dp0
+CD..
+python -m venv venv
+venv\scripts\pip install -r requirements.txt
 
 @REM Open Task Scheduler
 taskschd.msc
